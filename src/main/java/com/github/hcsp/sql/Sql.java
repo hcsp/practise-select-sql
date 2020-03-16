5c6c100c0a54082ff0463defec7bb55cfca53810
huiquan0509 <1076883793@qq.com>
2020-03-16 09:40:08 +0800

    SQL查询2

--- src/main/java/com/github/hcsp/sql/Sql.java
+++ src/main/java/com/github/hcsp/sql/Sql.java
@@ -72,7 +72,7 @@ public class Sql {
     /**
      * 题目1：
      * 查询有多少所有用户曾经买过指定的商品
-     *
+     * @param databaseConnection 数据库连接
      * @param goodsId 指定的商品ID
      * @return 有多少用户买过这个商品
      */
@@ -103,6 +103,7 @@ public class Sql {
      *
      * @param pageNum  第几页，从1开始
      * @param pageSize 每页有多少个元素
+     * @param databaseConnection 数据库
      * @return 指定页中的用户
      */
 // 例如，pageNum = 2, pageSize = 3（每页3个元素，取第二页），则应该返回：
@@ -159,6 +160,8 @@ public class Sql {
     /**
      * 题目3：
      * 查询所有的商品及其销售额，按照销售额从大到小排序
+     * @param databaseConnection 数据库连接
+     * @return 结果
      */
 // 预期的结果应该如图所示
 //  +----+--------+------+
@@ -216,6 +219,8 @@ public class Sql {
     /**
      * 题目4：
      * 查询订单信息，只查询用户名、商品名齐全的订单，即INNER JOIN方式
+     * @param databaseConnection 数据库连接
+     * @return 结果
      */
 // 预期的结果为：
 // +----------+-----------+------------+-------------+
@@ -261,6 +266,8 @@ public class Sql {
     /**
      * 题目5：
      * 查询所有订单信息，哪怕它的用户名、商品名缺失，即LEFT JOIN方式
+     * @param databaseConnection 数据库连接
+     * @return 结果
      */
 // 预期的结果为：
 // +----------+-----------+------------+-------------+
